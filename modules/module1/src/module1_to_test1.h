7716ffcdf8c4b79fa9d67cc1eaab580bb4ce964e
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-10-04 22:50:32 +0300

    gtest fixture test

--- modules/module1/src/module1_to_test1.h
+++ modules/module1/src/module1_to_test1.h
@@ -1,6 +1,20 @@
 #ifndef MODULE1_TO_TEST1
 #define MODULE1_TO_TEST1
 
+#include <queue>
+
 bool foo_to_test(void);
+
+class Module1 {
+    public:
+    std::size_t size() const {
+        return q_.size();
+    }
+    void push(const int e) {
+        q_.push(e);
+    }
+    private:
+    std::queue<int> q_;
+};
 
 #endif
--- test/module1/src/module1_test.cpp
+++ test/module1/src/module1_test.cpp
@@ -2,6 +2,18 @@
 
 #include "module1_to_test1.h"
 
-TEST(TestSuiteNameModule1ToTest1, TestNameFooToTest) {
+class Module1Test: public ::testing::Test {
+    protected:
+    void SetUp() override {
+        mq_.push(1);
+    }
+    Module1 mq_;
+};
+
+TEST(TestSuiteNameModule1ToTest1, TestNameFooToTest1) {
     EXPECT_TRUE(foo_to_test());
+}
+
+TEST_F(Module1Test, TestNameModule1Size) {
+    EXPECT_EQ(mq_.size(), 1);
 }
